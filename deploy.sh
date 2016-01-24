4ba793e3503a0ffdab3c4e7988d71426ec28c4d8
Alex Lubneuski <alex.lubneuski@gmail.com>
2016-01-23 23:21:51 -0500

    Initial Commit

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export AWS_ACCESS_KEY_ID='supersecret';
+export AWS_SECRET_ACCESS_KEY='supersecret';
+
+if [[ $1 == '' || $2 == '' ]]; then 
+   echo " "
+   echo "############# Deployment Script for notinphilly ###############"
+   echo "Please use the following format:";
+   echo "./deploy [application_elevation_level] [whether you want to deploy app - appDeploy]";
+   echo " "
+fi
+
+if [[ $2 == 'appDeploy' && $1 != '' ]]; then
+    ansible-playbook -i playbook/inventory/devservers -e "env="$1 --private-key=rsa.pem playbook/app_install.yml ;
+fi
+
+if [[ $2 == '' && $1 != '' ]]; then
+   ansible-playbook -i playbook/inventory/localhost -e "env="$1 --private-key=rsa.pem playbook/site.yml
+fi
